90c51df992f2d8dc6254194a944c7263fd10281a
Cosmin Pipirigeanu <cosbizmedia@gmail.com>
2017-06-27 17:50:14 +0300

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create! :first_name => 'Cosmin', :email => 'cosminstudio@gmail.com', :password => 'mihalache1988', :password_confirmation => 'mihalache1988', :role => 'admin'
+ 
+p "Created #{User.count} users"
