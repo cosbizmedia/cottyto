cd7a6eee776a694b415319535feff57240f2dfad
Cosmin Pipirigeanu <cosbizmedia@gmail.com>
2017-06-27 18:16:41 +0300

    fixing carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,15 +1,13 @@
 if Rails.env.production?
 	CarrierWave.configure do |config|
-		credentials = {
+		config.fog_credentials = {
 			:provider => 'AWS',
 			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
-			:aws_secret_access_key => ENV['S3_SECRET_KEY']
+			:aws_secret_access_key => ENV['S3_SECRET_KEY'],
+			:aws_signature_version => 2
 		}
 
-		storage = Fog::Storage.new(credentials)
-		storage.directories.each do |dir|
-    		puts "#{dir.key} (#{dir.location})"
-		end
+		config.fog_directory = ENV['S3_BUCKET']
 
 	end
 end
